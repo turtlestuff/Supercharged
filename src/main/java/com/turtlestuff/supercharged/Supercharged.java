66be8e973adfd83d1e5b558589532fa781478913
aren-cllc <106444@leerling.noordik.nl>
2019-07-23 00:31:57 +0200

    Initial commit

---
+++ src/main/java/com/turtlestuff/supercharged/Supercharged.java
@@ -0,0 +1,35 @@
+package com.turtlestuff.supercharged;
+
+import com.turtlestuff.supercharged.proxy.IProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "supercharged", useMetadata = true)
+public class Supercharged
+{
+
+    @SidedProxy(clientSide = "com.turtlestuff.supercharged.proxy.ClientProxy", serverSide = "com.turtlestuff.supercharged.proxy.ServerProxy")
+    public static IProxy proxy;
+
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        proxy.preInit(event);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        proxy.init(event);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+}
