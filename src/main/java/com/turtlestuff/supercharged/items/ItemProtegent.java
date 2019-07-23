9b1c659d53806c7f357ea32697eef003083625d4
reflectronic <john-tur@outlook.com>
2019-07-23 14:58:17 -0400

    Fix protegent item

--- src/main/java/com/turtlestuff/supercharged/items/ItemProtegent.java
+++ src/main/java/com/turtlestuff/supercharged/items/ItemProtegent.java
@@ -1,38 +1,53 @@
 package com.turtlestuff.supercharged.items;
 
 import com.turtlestuff.supercharged.init.ModItems;
-import com.turtlestuff.supercharged.items.ItemDeeta;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 
-public class ItemProtegent extends Item {
+import java.util.stream.IntStream;
 
-    public ItemProtegent() {
+public class ItemProtegent extends Item
+{
+
+    public ItemProtegent()
+    {
         super();
         setMaxStackSize(1);
         setCreativeTab(ModItems.superchargedTab);
     }
 
     @Override
-    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
-        boolean hasAnyDeeta = false;
-        for(ItemStack i : playerIn.inventory.mainInventory){
-            if(i.isItemEqual(new ItemStack(new ItemDeeta()))) {
-                hasAnyDeeta = true;
-            }
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        if (worldIn.isRemote) return super.onItemRightClick(worldIn, playerIn, handIn);
+
+        NonNullList<ItemStack> inventory = playerIn.inventory.mainInventory;
+        int[] indices =  IntStream.range(0, inventory.size())
+                .filter(i -> inventory.get(i) != null && inventory.get(i).getItem() instanceof ItemDeeta)
+                .toArray();
+
+        for (int index : indices)
+        {
+            playerIn.inventory.setInventorySlotContents(index, ItemStack.EMPTY);
         }
-        if(!hasAnyDeeta){
+
+        boolean hasAnyDeeta = indices.length != 0;
+
+        if (!hasAnyDeeta)
+        {
             playerIn.sendMessage(new TextComponentString("I HAVE NO DEETAAAAAAAAAAAAAAAAAAAAAAAAA TO PROTeCT!"));
-        } else {
+        }
+        else
+        {
             playerIn.sendMessage(new TextComponentString("LOTS OF DEETA FOR ME TO PROTEC1111 SECURITY IS MY MOTTO!"));
         }
+
         return super.onItemRightClick(worldIn, playerIn, handIn);
     }
-
-
 }
