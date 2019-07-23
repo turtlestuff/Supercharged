0c75a8b30257fe12ce2f9e9eb72c30bc5116f654
aren-cllc <106444@leerling.noordik.nl>
2019-07-23 19:41:52 +0200

    Add test block

--- src/main/java/com/turtlestuff/supercharged/EventHandler.java
+++ src/main/java/com/turtlestuff/supercharged/EventHandler.java
@@ -1,7 +1,11 @@
 package com.turtlestuff.supercharged;
 
+import com.turtlestuff.supercharged.blocks.TestBlock;
+import com.turtlestuff.supercharged.init.ModBlocks;
 import com.turtlestuff.supercharged.items.ItemProtegent;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -12,6 +16,12 @@ public class EventHandler {
     @SubscribeEvent
     public static void registerItem(RegistryEvent.Register<Item> event) {
         event.getRegistry().register(new ItemProtegent().setRegistryName("protegent").setUnlocalizedName("supercharged.protegent"));
-
+        event.getRegistry().register(new ItemBlock(ModBlocks.testblock).setRegistryName(ModBlocks.testblock.getRegistryName()));
     }
+
+    @SubscribeEvent
+    public static void registerBlock(RegistryEvent.Register<Block> event) {
+        event.getRegistry().register(new TestBlock());
+    }
+
 }
